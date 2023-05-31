f01615c8fb896a5713c057f186f366d3a3bcac59
RomoliGabriele <gabriele.romoli@comm-it.it>
2023-05-31 17:05:50 +0200

    Tabella funzionante

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,174 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <table-lite
+      :is-loading="table.isLoading"
+      :columns="table.columns"
+      :rows="table.rows"
+      :total="table.totalRecordCount"
+      :sortable="table.sortable"
+      @do-search="doSearch"
+      @is-finished="tableLoadingFinish"
+    />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import TableLite from "vue3-table-lite";
+import axios from "axios";
+import { defineComponent, reactive, ref, computed, createApp, h } from "vue";
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+const searchTerm = ref(""); // Search text
+const searchTerm2 = ref(""); // Search text
+
+export default {
+  components: { TableLite },
+  setup() {
+    // Init Your table settings
+    const table = reactive({
+      isLoading: false,
+      columns: [
+        {
+          label: "STATO DOC",
+          field: "is_read",
+          width: "10%",
+        },
+        {
+          label: "TIPO DOC",
+          field: "type",
+          width: "5%",
+          sortable: true,
+        },
+        {
+          label: "NOME DOCUMENTO",
+          field: "name",
+          width: "20%",
+          sortable: true,
+        },
+        {
+          label: "NUMERO DOCUMENTO",
+          field: "id",
+          width: "3%",
+          sortable: true,
+          isKey: true,
+        },
+        {
+          label: "DATA DOC",
+          field: "date",
+          width: "5%",
+          sortable: true,
+        },
+        {
+          label: "CLIENTE COD",
+          field: "client_id",
+          width: "5%",
+          sortable: true,
+        },
+        {
+          label: "VISIBILE FINO AL",
+          field: "due_date",
+          width: "5%",
+          sortable: true,
+        },
+        {
+          label: "TIPO FILE",
+          field: "file_type",
+          width: "5%",
+          sortable: false,
+        },
+        {
+          label: "FILE (KB)",
+          field: "file_size",
+          width: "5%",
+          sortable: true,
+        },
+      ],
+      rows: computed(() => {
+        return data.filter(
+          (x) => x.name.toLowerCase().includes(searchTerm2.value.toLowerCase()) //&&
+          //x.name.toLowerCase().includes(searchTerm.value.toLowerCase())
+        );
+      }),
+      totalRecordCount: computed(() => {
+        return table.rows.length;
+      }),
+      sortable: {
+        order: "id",
+        sort: "asc",
+      },
+    });
+
+    /**
+     * Table search event
+     */
+    const doSearch = (offset, limit, order, sort) => {
+      table.isLoading = true;
+
+      // Start use axios to get data from Server
+      let url =
+        "http://localhost/api/health" +
+        offset +
+        "&limit=" +
+        limit +
+        "&order=" +
+        order +
+        "&sort=" +
+        sort;
+      axios.get(url).then((response) => {
+        // Point: your response is like it on this example.
+        response = {
+          rows: [
+            {
+              is_read: "DA LEGGERE",
+              id: 5264627,
+              name: "DDT20254692-01245",
+              type: "FAT",
+              date: "2021-01-01",
+              client_id: 123456,
+              file_type: "PDF",
+              due_date: "2021-01-31",
+              file_size: 123.8,
+            },
+            {
+              is_read: "LETTO",
+              id: 23952750,
+              name: "DDT21254692-02403",
+              type: "DDT",
+              date: "2021-01-01",
+              client_id: 123456,
+              file_type: "PDF",
+              due_date: "2021-01-31",
+              file_size: 13.8,
+            },
+          ],
+          count: 2,
+        };
+
+        // refresh table rows
+        table.rows = response.rows;
+        table.totalRecordCount = response.count;
+        table.sortable.order = order;
+        table.sortable.sort = sort;
+
+        tableLoadingFinish();
+      });
+      // End use axios to get data from Server
+    };
+
+    /**
+     * Table search finished event
+     */
+    const tableLoadingFinish = (elements) => {
+      table.isLoading = false;
+    };
+
+    // Get data first
+    doSearch(0, 10, "id", "asc");
+
+    return {
+      table,
+      doSearch,
+      tableLoadingFinish,
+    };
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,256 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <table-lite
+    :is-loading="table.isLoading"
+    :columns="table.columns"
+    :rows="table.rows"
+    :total="table.totalRecordCount"
+    :sortable="table.sortable"
+    :page="table.page"
+    @do-search="doSearch"
+    @VnodeMounted="initTable"
+    @is-finished="table.isLoading = false"
+  ></table-lite>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent, reactive, ref, computed, createApp, h } from "vue";
+import TableLite from "vue3-table-lite";
+import axios from "axios";
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+const id = ref(""); // Search text
+const is_read = ref(""); // select
+const type = ref(""); // select
+const from = ref(); // date
+const to = ref(); // date
+
+var rows = [];
+
+export default defineComponent({
+  name: "App",
+  components: { TableLite },
+  data() {
+    return {
+      rows: [],
+      table: {},
+    };
+  },
+  setup() {
+    // Table config
+    const table = reactive({
+      isLoading: false,
+      columns: [
+        {
+          label: "STATO DOC",
+          field: "is_read",
+          width: "10%",
+        },
+        {
+          label: "TIPO DOC",
+          field: "type",
+          width: "5%",
+          sortable: true,
+        },
+        {
+          label: "NOME DOCUMENTO",
+          field: "name",
+          width: "20%",
+        },
+        {
+          label: "NUMERO DOCUMENTO",
+          field: "id",
+          width: "3%",
+          sortable: true,
+          isKey: true,
+        },
+        {
+          label: "DATA DOC",
+          field: "date",
+          width: "10%",
+          sortable: true,
+        },
+        {
+          label: "CLIENTE COD",
+          field: "customer_id",
+          width: "5%",
+        },
+        {
+          label: "VISIBILE FINO AL",
+          field: "due_date",
+          width: "5%",
+        },
+        {
+          label: "TIPO FILE",
+          field: "file_type",
+          width: "5%",
+        },
+        {
+          label: "FILE (KB)",
+          field: "file_size",
+          width: "5%",
+        },
+      ],
+      rows: [],
+      totalRecordCount: 0,
+      sortable: {
+        order: "id",
+        sort: "asc",
+      },
+      page: 1,
+    });
+
+    const doSearch = async (offset, limit, order, sort) => {
+      table.isLoading = true;
+      // Start use axios to get data from Server
+      let url =
+        "http://localhost:8010/api/documents?offset=" +
+        offset +
+        "&limit=" +
+        limit +
+        "&order=" +
+        order +
+        "&sort=" +
+        sort;
+      let response = await axios.get(url);
+      console.log();
+      table.rows = computed(() => {
+        if (!from.value || !to.value) {
+          return response.data.rows.filter(
+            (x) =>
+              String(x.id).toLowerCase().includes(id.value.toLowerCase()) &&
+              String(x.is_read)
+                .toLowerCase()
+                .includes(is_read.value.toLowerCase()) &&
+              String(x.type).toLowerCase().includes(type.value.toLowerCase())
+          );
+        } else {
+          return response.data.rows.filter(
+            (x) =>
+              String(x.id).toLowerCase().includes(id.value.toLowerCase()) &&
+              String(x.is_read)
+                .toLowerCase()
+                .includes(is_read.value.toLowerCase()) &&
+              String(x.type).toLowerCase().includes(type.value.toLowerCase()) &&
+              Date.parse(x.date) >= Date.parse(from.value) &&
+              Date.parse(x.date) <= Date.parse(to.value)
+          );
+        }
+      });
+      table.totalRecordCount = response.data.total;
+      table.sortable.order = order;
+      table.sortable.sort = sort;
+      table.isLoading = false;
+      table.page = offset / limit + 1;
+      console.log(table.page);
+      // End use axios to get data from Server
+    };
+
+    doSearch(0, 10, "id", "asc");
+    /**
+     * Trigger after initail component
+     */
+    const initTable = ({ el: tableComponent }) => {
+      let headerTr = tableComponent.getElementsByClassName("vtl-thead-tr");
+      if (!headerTr[0]) {
+        return;
+      }
+      let cloneTr = headerTr[0].cloneNode(true); // Clone first <tr>
+      let childTh = cloneTr.getElementsByClassName("vtl-thead-th");
+      for (let i = 0; i < childTh.length; i++) {
+        // Clear <th>'s HTML
+        childTh[i].innerHTML = "";
+      }
+      createApp(
+        defineComponent({
+          setup() {
+            return () =>
+              h(
+                "select",
+                {
+                  onInput: (e) => {
+                    is_read.value = e.target.value;
+                  },
+                },
+                [
+                  h("option", { value: "" }, "Tutti"),
+                  h("option", { value: "LETTO" }, "Letto"),
+                  h("option", { value: "DA LEGGERE" }, "Da leggere"),
+                ]
+              );
+          },
+        })
+      ).mount(childTh[0]);
+
+      createApp(
+        defineComponent({
+          setup() {
+            return () =>
+              h(
+                "select",
+                {
+                  onInput: (e) => {
+                    type.value = e.target.value;
+                  },
+                },
+                [
+                  h("option", { value: "" }, "Tutti"),
+                  h("option", { value: "FAT" }, "FAT"),
+                  h("option", { value: "DDT" }, "DDT"),
+                ]
+              );
+          },
+        })
+      ).mount(childTh[1]);
+      // Create a input element and append to first <th>
+      createApp(
+        defineComponent({
+          setup() {
+            return () =>
+              h("input", {
+                type: "number",
+                value: id.value,
+                onInput: (e) => {
+                  id.value = e.target.value;
+                },
+              });
+          },
+        })
+      ).mount(childTh[3]);
+      // append cloned element to the header after first <tr>
+      headerTr[0].after(cloneTr);
+
+      createApp(
+        defineComponent({
+          setup() {
+            return () => [
+              h("label", "DA: "),
+              h("input", {
+                type: "date",
+                value: from.value,
+                onInput: (e) => {
+                  from.value = e.target.value;
+                },
+              }),
+              h("br"),
+              h("label", "A: "),
+              h("input", {
+                type: "date",
+                value: to.value,
+                onInput: (e) => {
+                  to.value = e.target.value;
+                },
+              }),
+            ];
+          },
+        })
+      ).mount(childTh[4]);
+    };
+
+    return {
+      id,
+      table,
+      initTable,
+      doSearch,
+    };
+  },
+});
 </script>
